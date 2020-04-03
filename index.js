d3d4724478f95cabb51b2b8e40cb45fb741e0587
idena-dev <hello@idena.dev>
2020-04-03 08:16:53 +0200

    whitelist as config

--- index.js
+++ index.js
@@ -1,43 +1,6 @@
 const app = require("express")();
 const proxy = require("express-http-proxy");
-const { target, apiKey, port } = require("./config");
-
-const whitelist = [
-
-    // List all identities (not only validated ones)
-    "dna_identities",
-
-    // Show info about identity for given address
-    "dna_identity",
-
-    // Details about the current epoch
-    "dna_epoch",
-
-    // Show info about validation ceremony
-    "dna_ceremonyIntervals",
-
-    // Show DNA balance for address
-    "dna_getBalance",
-
-    // Details about a specific transaction
-    "bcn_transaction",
-
-    // List specific number of transactions for given address
-    "bcn_transactions",
-    
-    // List specific number of pending transactions for given address
-    "bcn_pendingTransactions",
-
-    // Show if node is synchronized
-    "bcn_syncing",
-
-    // Show node version
-    "dna_version",
-
-    // https://github.com/idena-network/idena-go/blob/85b088d704fe6b2dcd8832df2da598b5f729ea06/api/blockchain_api.go#L208
-    "bcn_SendRawTx",
-
-];
+const { target, apiKey, port, whitelist } = require("./config");
 
 function tryParseJSON (rawJson) {
     try {
