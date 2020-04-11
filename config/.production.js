344dd6113922f819c0842e83c5c6f205dd1f3ac8
idena-dev <hello@idena.dev>
2020-04-11 10:47:23 +0200

    update whitelist default production config

--- config/.production.js
+++ config/.production.js
@@ -4,38 +4,29 @@ module.exports = {
     port: 80,
     whitelist: [
 
-        // List all identities (not only validated ones)
-        "dna_identities",
-    
-        // Show info about identity for given address
-        "dna_identity",
-    
-        // Details about the current epoch
-        "dna_epoch",
-    
-        // Show info about validation ceremony
-        "dna_ceremonyIntervals",
-    
-        // Show DNA balance for address
-        "dna_getBalance",
-    
-        // Details about a specific transaction
-        "bcn_transaction",
-    
-        // List specific number of transactions for given address
-        "bcn_transactions",
-        
-        // List specific number of pending transactions for given address
-        "bcn_pendingTransactions",
-    
-        // Show if node is synchronized
-        "bcn_syncing",
-    
-        // Show node version
-        "dna_version",
-    
-        // https://github.com/idena-network/idena-go/blob/85b088d704fe6b2dcd8832df2da598b5f729ea06/api/blockchain_api.go#L208
-        "bcn_SendRawTx",
-    
+	    // Show info about identity for given address
+	    "dna_identity",
+	    
+	    // Details about the current epoch
+	    "dna_epoch",
+	    
+	    // Show DNA balance for address
+	    "dna_getBalance",
+	    
+	    // Details about a specific transaction
+	    "bcn_transaction",
+	    
+	    // List specific number of transactions for given address
+	    "bcn_transactions",
+	    
+	    // List specific number of pending transactions for given address
+	    "bcn_pendingTransactions",
+	    
+	    // Show if node is synchronized
+	    "bcn_syncing",
+	    
+	    // https://github.com/idena-network/idena-go/blob/85b088d704fe6b2dcd8832df2da598b5f729ea06/api/blockchain_api.go#L208
+	    "bcn_SendRawTx",
+
     ]
 };
