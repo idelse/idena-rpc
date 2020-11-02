a9236e20eddd1fb8b6a1c6a893e490f9befef879
Mahmoud Mohamed <61373609+RealMahmoud@users.noreply.github.com>
2020-11-02 17:03:14 +0200

    Add more methods

--- config/.production.js
+++ config/.production.js
@@ -1,6 +1,29 @@
 module.exports = {
-    target: "http://localhost:9009",
-    apiExplorer: "https://api.idena.org",
-    apiKey: "<insert content of datadir/api.key file>",
-    port: 80,
+  target: "http://localhost:9009",
+  apiExplorer: "https://api.idena.org",
+  apiKey: "<insert content of datadir/api.key file>",
+  port: 80,
+  whitelist: [
+    "dna_identity",
+    "dna_epoch",
+    "dna_getBalance",
+    "bcn_transaction",
+    "bcn_transactions",
+    "bcn_pendingTransactions",
+    "bcn_syncing",
+    "bcn_sendRawTx",
+    "bcn_feePerGas",
+    'dna_ceremonyIntervals',
+    'dna_isValidationReady',
+    'dna_wordsSeed',
+    'flip_getRaw',
+    'flip_getKeys',
+    'flip_words',
+    'flip_shortHashes',
+    'flip_longHashes',
+    'flip_privateEncryptionKeyCandidates',
+    'flip_sendPrivateEncryptionKeysPackage',
+    'flip_sendPublicEncryptionKey',
+    'bcn_getRawTx'
+  ]
 };
